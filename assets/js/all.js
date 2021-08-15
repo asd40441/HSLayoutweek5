772065566fd4d1b6859a8a7c72ef375ab758f2fe
asd40441 <asd40441@hotmail.com>
2021-08-15 14:11:47 +0800

    Update 2021-08-15T06:11:46.054Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,6 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+ClassicEditor.create(document.querySelector('#editor'))["catch"](function (error) {
+  console.error(error);
 });
 //# sourceMappingURL=all.js.map
