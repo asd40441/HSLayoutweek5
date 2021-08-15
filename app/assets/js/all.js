82cfc6689f33a4ed03c4c8e78bfc94a5e03aa519
asd40441 <asd40441@hotmail.com>
2021-08-15 14:11:30 +0800

    finish one page

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,5 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+ClassicEditor
+        .create( document.querySelector( '#editor' ) )
+        .catch( error => {
+            console.error( error );
+        } );
